403acecbc2b481a153786a3d2a733a189061a695
七只蜗牛 <zuoyanjide@163.com>
2026-05-07 21:09:36 +0800

    Add files via upload

---
+++ source/ErrorReporter.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <string>
+#include <functional>
+
+namespace YGsoftware
+{
+    // 启动时设置一次
+    void SetErrorHandler(std::function<void(const std::string&)> handler);
+
+    // 全局调用
+    void ReportError(const std::string& msg);
+}
