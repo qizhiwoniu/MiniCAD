57201b2796b9e845b3d306b9d8d10b6eb938a67f
七只蜗牛 <zuoyanjide@163.com>
2026-05-15 17:57:31 +0800

    1.1.0

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 //  版本号 —— 只在此处修改，其他文件全部引用这里
 // ============================================================
 #define APP_VERSION_MAJOR   1
-#define APP_VERSION_MINOR   0
-#define APP_VERSION_PATCH   3
+#define APP_VERSION_MINOR   1
+#define APP_VERSION_PATCH   0
 #define APP_VERSION_BUILD   0
 
 
