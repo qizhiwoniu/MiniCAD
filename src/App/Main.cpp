ab252bf9bfd4a3c618a7df6627080bb21f557935
七只蜗牛 <zuoyanjide@163.com>
2026-05-15 10:57:57 +0800

    1.0.2新版本

--- src/App/Main.cpp
+++ src/App/Main.cpp
@@ -4,31 +4,7 @@
 
 using namespace MiniCAD;
 
-//int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE, PSTR, int)
-//{
-//    //1. 先创建托盘图标
-//    TrayIcon tray(hInst, L"MiniCAD - 就绪");
-//    tray.Create();
-//    //2. 用系统内置图标（暂时，之后换成你自己的.ico）
-//    HICON iconNormal = LoadIcon(nullptr, IDI_APPLICATION);  // 普通状态
-//    HICON iconAlert = LoadIcon(nullptr, IDI_INFORMATION);  // 有消息时
-//    //3. 初始化主窗口
-//    MainWindow mainWindow;
-//    mainWindow.Initialize(L"MiniCAD", 600, 400);
-//    //4. 示例：窗口启动后弹出气泡通知
-//    tray.ShowBalloon(L"MiniCAD 已启动", L"程序正在运行中...");
-//
-//    mainWindow.Run();
-//    return 0;
-//
-//
-//    //tray.StartBlink(iconNormal, iconAlert, 500);  // 500ms 闪一次
-//
-//    // 消息已读，停止闪烁：
-//    //tray.StopBlink();
-//}
- 
-int main(int argc, char* argv[]) 
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE, PSTR, int)
 {
     //1. 先创建托盘图标
     TrayIcon tray(hInst, L"MiniCAD - 就绪");
@@ -51,3 +27,18 @@ int main(int argc, char* argv[])
     // 消息已读，停止闪烁：
     //tray.StopBlink();
 }
+ 
+//int main(int argc, char* argv[]) 
+//{
+//    SetConsoleOutputCP(CP_UTF8);
+//    SetConsoleCP(CP_UTF8);
+//    // 初始化主窗口
+//    MainWindow mainWindow;
+//    mainWindow.Initialize(L"MiniCAD", 600, 400);
+//
+//
+//    mainWindow.Run();
+//    return 0;
+//
+//
+//}
